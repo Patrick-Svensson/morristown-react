01771da7f9a20c63fae3eaf9b97f8bc5b44a35a3
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-16 18:06:51 +0200

    Add dropdown menu toggler icon.

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -7,6 +7,16 @@ const Navbar = () => {
             <a href="#">
                 <img className="navbar__logo-container" src={image} />
             </a>
+            <a class="navbar__toggler" href="#">
+                <span>
+                    <i
+                        className="fas fa-align-justify"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    ></i>
+                </span>
+            </a>
         </nav>
     );
 };
